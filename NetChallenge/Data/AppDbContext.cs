a15d21d97371ab65174216587956cff1211fd29b
Qimono-Desktop-W11 <alberto.gruning.zen@gmail.com>
2023-11-08 13:20:55 -0300

    feat:add migrations for ddbb versioning

--- NetChallenge/Data/AppDbContext.cs
+++ NetChallenge/Data/AppDbContext.cs
@@ -4,6 +4,6 @@ using NetChallenge.Domain;
 namespace NetChallenge.Data{
     public class AppDbContext:DbContext{
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options){}
-        public DbSet<Location> Locations { get; set; }
+        public DbSet<Location> Locations => Set<Location>();
     }
 }
